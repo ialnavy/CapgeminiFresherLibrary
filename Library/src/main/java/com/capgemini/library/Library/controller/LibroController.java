0f5c274ffea242f1d7db0f396285a25e9e7528ab
Iván Álvarez López <ivan.alvarez-lopez@capgemini.com>
2023-05-08 17:18:12 +0200

    List libro controller

--- Library/src/main/java/com/capgemini/library/Library/controller/LibroController.java
+++ Library/src/main/java/com/capgemini/library/Library/controller/LibroController.java
@@ -1,39 +1,17 @@
 package com.capgemini.library.Library.controller;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.capgemini.library.Library.model.Libro;
-
 
 @Controller
 public class LibroController {
 
-    @Autowired
-    private LibroRepository libroRepository;
+	@GetMapping("/libro/list")
+	public String listarLibros(Model model) {
+//		List<Libro> libros = libroService.readAll();
+//		model.addAttribute("libros", libros);
 
-    @GetMapping("/añadir-libro")
-    public String mostrarFormulario(Model model) {
-        model.addAttribute("libro", new Libro());
-        return "añadir-libro";
-    }
-
-    @PostMapping("/añadir-libro")
-    public String añadirLibro(@ModelAttribute Libro libro) {
-        libroRepository.save(libro);
-        return "redirect:/lista-libros";
-    }
-
-    @GetMapping("/lista-libros")
-    public String listarLibros(Model model) {
-        List<Libro> libros = libroRepository.findAll();
-        model.addAttribute("libros", libros);
-        return "lista-libros";
-    }
+		return "listLibro";
+	}
 }
-
