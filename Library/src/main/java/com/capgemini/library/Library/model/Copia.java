9b7be896c0e6be928de6d4c747adc2a063fa8319
Iván Álvarez López <ivan.alvarez-lopez@capgemini.com>
2023-05-08 16:19:44 +0200

    Mod Copia

--- Library/src/main/java/com/capgemini/library/Library/model/Copia.java
+++ Library/src/main/java/com/capgemini/library/Library/model/Copia.java
@@ -1,8 +1,57 @@
 package com.capgemini.library.Library.model;
 
-public class Copia {
-	
-	private Identifier id;
-	
-	private estadoCopia estado;
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Copia implements Serializable {
+
+	private static final long serialVersionUID = -1645129037600814474L;
+
+	@Id
+	@Column
+	private String id = UUID.randomUUID().toString();
+
+	@Enumerated(EnumType.STRING)
+	private EstadoCopia estado;
+
+	public Copia() {
+		super();
+	}
+
+	public Copia(String id, EstadoCopia estado) {
+		super();
+		this.id = id;
+		this.estado = estado;
+	}
+
+	public Copia(EstadoCopia estado) {
+		super();
+		this.estado = estado;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public EstadoCopia getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoCopia estado) {
+		this.estado = estado;
+	}
+
 }
