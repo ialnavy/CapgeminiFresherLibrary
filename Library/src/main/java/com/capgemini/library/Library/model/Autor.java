026adfd5cae6ef910cf43fa3e1543f25b831b5a1
Iván Álvarez López <ivan.alvarez-lopez@capgemini.com>
2023-05-08 16:06:53 +0200

    Autor completado

--- Library/src/main/java/com/capgemini/library/Library/model/Autor.java
+++ Library/src/main/java/com/capgemini/library/Library/model/Autor.java
@@ -1,5 +1,6 @@
 package com.capgemini.library.Library.model;
 
+import java.sql.Date;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -10,5 +11,65 @@ public class Autor {
 	@Id
 	@Column
 	private String id = UUID.randomUUID().toString();
+
+	@Column
+	private String nombre;
+
+	@Column
+	private String nacionalidad;
+
+	@Column
+	private Date fechaNacimiento;
+
+	public Autor() {
+		super();
+	}
+
+	public Autor(String id, String nombre, String nacionalidad, Date fechaNacimiento) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.nacionalidad = nacionalidad;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Autor(String nombre, String nacionalidad, Date fechaNacimiento) {
+		super();
+		this.nombre = nombre;
+		this.nacionalidad = nacionalidad;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
 
 }
