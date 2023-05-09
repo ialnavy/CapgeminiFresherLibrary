66ad739747608ba22debe24d21da0073bbb5124c
Iván Álvarez López <ivan.alvarez-lopez@capgemini.com>
2023-05-09 10:15:20 +0200

    Arreglo de errores

--- Library/src/main/java/com/capgemini/library/Library/controller/LibroController.java
+++ Library/src/main/java/com/capgemini/library/Library/controller/LibroController.java
@@ -1,16 +1,25 @@
 package com.capgemini.library.Library.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.capgemini.library.Library.model.Libro;
+import com.capgemini.library.Library.service.LibroService;
+
 @Controller
 public class LibroController {
 
+	@Autowired
+	private LibroService libroService;
+
 	@GetMapping("/libro/list")
 	public String listarLibros(Model model) {
-//		List<Libro> libros = libroService.readAll();
-//		model.addAttribute("libros", libros);
+		List<Libro> libros = libroService.obtenerTodosLosLibros();
+		model.addAttribute("libros", libros);
 
 		return "listLibro";
 	}
