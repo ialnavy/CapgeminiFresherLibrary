da1c4a338f08e46a2c29a945fab98864220b3a1c
Iván Álvarez López <ivan.alvarez-lopez@capgemini.com>
2023-05-12 11:12:26 +0200

    Creación de login y register

--- Library/src/main/java/com/capgemini/library/Library/WebSecurityConfig.java
+++ Library/src/main/java/com/capgemini/library/Library/WebSecurityConfig.java
@@ -3,20 +3,32 @@ package com.capgemini.library.Library;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.capgemini.library.Library.service.MyUserDetailsService;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
+
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeHttpRequests().requestMatchers("/", "/register", "/login").permitAll()
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll().and().httpBasic();
+		return http.build();
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll());
-		return http.build();
+	UserDetailsService userDetailsService() {
+		return new MyUserDetailsService();
 	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new Sha512PasswordEncoder();
+	}
+
 }
