b1c61ffc266f174127791e92b59520bbe856737b
Ignacio Angulo Couso <ignacio.angulo-couso@capgemini.com>
2023-05-15 16:33:50 +0200

    reservas 90%

--- Library/src/main/java/com/capgemini/library/repository/ReservaRepository.java
+++ Library/src/main/java/com/capgemini/library/repository/ReservaRepository.java
@@ -1,9 +1,13 @@
 package com.capgemini.library.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.capgemini.library.model.Reserva;
 
 public interface  ReservaRepository  extends CrudRepository<Reserva, String>{
+
+    List<Reserva> findByCopiaIdOrderByFechaReservaAsc(String copiaId);
 
 }
--- Library/src/main/java/com/capgemini/library/service/ReservaServiceImp.java
+++ Library/src/main/java/com/capgemini/library/service/ReservaServiceImp.java
@@ -1,5 +1,40 @@
 package com.capgemini.library.service;
 
-public class ReservaServiceImp implements ReservaService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.capgemini.library.model.Reserva;
+import com.capgemini.library.repository.ReservaRepository;
+
+@Service
+public class ReservaServiceImp implements ReservaService {
+    @Autowired
+    private ReservaRepository reservaRepository;
+
+    @Override
+    public Reserva createReserva(Reserva reserva) {
+        return reservaRepository.save(reserva);
+    }
+
+    @Override
+    public void cancelReserva(String id) {
+        reservaRepository.deleteById(id);
+    }
+
+    @Override
+    public void verifyReservas(String copiaId) {
+         List<Reserva> reservas = reservaRepository.findByCopiaIdOrderByFechaReservaAsc(copiaId);
+        if (!reservas.isEmpty()) {
+            Reserva reserva = reservas.get(0);
+            // Enviar un correo electrónico al Socio
+            // ...
+        }
+    }
+
+    @Override
+    public List<Reserva> getAllReservas() {
+        return (List<Reserva>) reservaRepository.findAll();
+    }
 }
