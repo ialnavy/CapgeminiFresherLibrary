04f8add1e0f4ea00090ecba18990827f4c2b48bd
Ignacio Angulo Couso <ignacio.angulo-couso@capgemini.com>
2023-05-16 16:35:04 +0200

    Fixing bugs(2)

--- Library/src/main/java/com/capgemini/library/service/model/PrestamoService.java
+++ Library/src/main/java/com/capgemini/library/service/model/PrestamoService.java
@@ -2,6 +2,7 @@ package com.capgemini.library.service.model;
 
 import java.util.List;
 
+import com.capgemini.library.ServiceException;
 import com.capgemini.library.model.Lector;
 import com.capgemini.library.model.Prestamo;
 import com.capgemini.library.service.AbstractService;
@@ -11,4 +12,7 @@ public interface PrestamoService extends AbstractService<Prestamo> {
 	public void verificarPrestamosYMultas();
 
 	public List<Prestamo> getPrestamosActivosByLector(Lector lector);
+	
+	public boolean isCreable(Prestamo prestamo, String LectorID) throws ServiceException;
+
 }
